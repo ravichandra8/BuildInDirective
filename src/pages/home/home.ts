7961b0d6d73f13a862dc03387fdcfd41f594a80f
lenovo <ravichandrachakka8@gmail.com>
2017-12-05 18:52:27 +0530

    inital commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,106 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  employees:any[];
+  display : boolean = false;
+  dataswitch:string;
   constructor(public navCtrl: NavController) {
-
+    this.dataswitch="ravi";
+this.employees=[
+  {
+         "DivisionId": "1",
+         "Divisions": "ABIDS"
+     },
+     {
+         "DivisionId": "14",
+         "Divisions": "ASIF NAGAR"
+     },
+     {
+         "DivisionId": "15",
+         "Divisions": "BANJARA HILLS"
+     }
+];
   }
+getEmployees():void
+{
+  this.employees=[
+    {
+           "DivisionId": "1",
+           "Divisions": "ABIDS"
+       },
+       {
+           "DivisionId": "14",
+           "Divisions": "ASIF NAGAR"
+       },
+       {
+           "DivisionId": "15",
+           "Divisions": "BANJARA HILLS"
+       },
+       {
+           "DivisionId": "9",
+           "Divisions": "BEGUMPET"
+       },
+       {
+           "DivisionId": "10",
+           "Divisions": "CHARMINAR"
+       },
+       {
+           "DivisionId": "2",
+           "Divisions": "CHIKKADAPALLY"
+       },
+       {
+           "DivisionId": "11",
+           "Divisions": "FALAKNUMA"
+       },
+       {
+           "DivisionId": "7",
+           "Divisions": "GOPALPURAM"
+       },
+       {
+           "DivisionId": "16",
+           "Divisions": "GOSHAMAHAL"
+       },
+       {
+           "DivisionId": "6",
+           "Divisions": "KACHIGUDA"
+       },
+       {
+           "DivisionId": "8",
+           "Divisions": "MAHANKALI"
+       },
+       {
+           "DivisionId": "4",
+           "Divisions": "MALAKPET"
+       },
+       {
+           "DivisionId": "12",
+           "Divisions": "MIRCHOWK"
+       },
+       {
+           "DivisionId": "17",
+           "Divisions": "PANJAGUTTA"
+       },
+       {
+           "DivisionId": "3",
+           "Divisions": "SAIFABAD"
+       },
+       {
+           "DivisionId": "13",
+           "Divisions": "SANTOSH NAGAR"
+       },
+       {
+           "DivisionId": "5",
+           "Divisions": "SULTAN BAZAR"
+       }
+  ];
+}
+trackByEmpCode(index:number,employee:any):string
+{
+  return employee.DivisionId;
+}
+showhide():void
+{
+  this.display=!this.display;
+}
 
 }
